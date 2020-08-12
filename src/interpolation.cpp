ebe41c6aca36ee01b011fa60352567f68bd59f85
Zhi Li <zhili@Zhis-MacBook-Pro.local>
2020-08-12 19:46:45 +0800

    save file problem

--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -96,7 +96,8 @@ void Interpolation::writeToJson(std::unordered_map<double, SensorType>& timestam
         std::string save_json_path = outputPath + camera_folders[timestamp_sensortype[iter->first]] + std::to_string(iter->first) + ".pcd.tar.json";
         //std::cout << save_json_path << std::endl;
         std::ofstream save_json_to_file(save_json_path);
-        save_json_to_file << jsonfiles;
+        //save_json_to_file << jsonfiles;
+        save_json_to_file.write(jsonfiles, save_json_to_file, pretty_print);
     }
     
 
